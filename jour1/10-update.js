cadeb180e893b33dbdbc92fd809c4ace969676f2
Malik H <malik.h@webdevpro.net>
2021-10-11 17:24:48 +0200

    update QueryFirst

--- jour1/10-update.js
+++ jour1/10-update.js
@@ -12,4 +12,28 @@ connect( process.env.URLBDD , { useNewUrlParser : true } )
 // 2 manières de faire
 // QueryFirst 
 
+async function modifierUtilisateur(id){
+    // recherché dans la base de données éléments qui a l'id recherché
+    // const utilisateurAModifier = await Utilisateur.findOne({ _id : id } )
+    const utilisateurAModifier = await Utilisateur.findById( id );
+
+    if(utilisateurAModifier){
+        // modifier et enresgistrer 
+        try{
+            utilisateurAModifier.nom = [];
+            utilisateurAModifier.age = "un peu de texte" ;
+            const resultat = await utilisateurAModifier.save()
+            console.log(resultat);
+        }catch(erreur){
+            for(let champ in erreur.errors){
+                console.log(erreur.errors[champ].message)
+            }
+        }
+        return ;
+    }
+    return console.log(`aucun utilisateur n'a l'id ${id}`);
+}
+
+modifierUtilisateur("61642fd0a38bca96513d6625");
+
 // UpdateFirst
