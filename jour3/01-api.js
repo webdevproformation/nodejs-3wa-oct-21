5d8b32fe8a52bbc3f23e9709eceef5ca1fe1ac2a
Malik H <malik.h@webdevpro.net>
2021-10-13 12:36:29 +0200

    commentaires

--- jour3/01-api.js
+++ jour3/01-api.js
@@ -1,12 +1,12 @@
 // cd jour3
 // npm init --yes package.json
-// npm i express mongoose dotenv
+// npm i express mongoose dotenv joi
 
 const express = require("express");
-
-require("./demarrage/bdd")();
 const app = express();
-require("./demarrage/routes")(app)
+
+require("./demarrage/bdd")(); // se connecter à la base de données
+require("./demarrage/routes")(app) // charger les routes express 
 
 const PORT = process.env.PORT || 5232;
 app.listen( PORT , () => console.log(`serveur express est démarré sur le port ${PORT}`))
