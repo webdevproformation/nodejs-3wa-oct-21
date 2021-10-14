7add72827bd822c84de42d250a1510992e731e6a
Malik H <malik.h@webdevpro.net>
2021-10-14 15:10:08 +0200

    exo 1

--- jour4-template/serveur.js
+++ jour4-template/serveur.js
@@ -67,6 +67,60 @@ app.get("/loop", (req, rep) => {
     rep.render("07-loop" , produits);
 })
 
+// créer un nouvelle route /exo => appeler fichier 08-tableau-de-bord.pug
+
+// ce fichier va extends un fichier pug template back-office 
+/**
+head
+body
+    inclure un template => menu 
+    block tableau-debord
+
+ */
+
+// fichier liste ul avec 3 li qui contient chacun Accueil / Présentation / Contact
+
+
+// 08-tableau-de-bord.pug 
+// tableau <table> qui va contenir la liste d'articles suivantes (qui est envoyée depuis le fichier server.js)
+
+/*
+[
+  {
+    "_id": "61682bf5e6dfd4b6594faa2d",
+    "titre": "do eu nisi voluptate occaecat",
+    "contenu": "Commodo sint ad aute aliquip aute cillum ad incididunt incididunt nostrud culpa proident."
+  },
+  {
+    "_id": "61682bf5855898e021007dd7",
+    "titre": "reprehenderit cillum ut veniam ullamco",
+    "contenu": "Cillum reprehenderit cillum enim cillum occaecat ullamco quis est esse commodo irure ea consectetur in."
+  },
+  {
+    "_id": "61682bf5e07bf1c3ff13df06",
+    "titre": "laboris sint officia eiusmod dolore",
+    "contenu": "Amet exercitation sint veniam fugiat eiusmod reprehenderit amet enim pariatur enim."
+  },
+  {
+    "_id": "61682bf58411e475e6e0d4f8",
+    "titre": "laboris proident commodo ea eu",
+    "contenu": "Duis cillum reprehenderit amet ea quis in."
+  },
+  {
+    "_id": "61682bf58f6ad826957550e8",
+    "titre": "ullamco anim consequat elit dolore",
+    "contenu": "Id pariatur ex ut culpa incididunt eu eiusmod."
+  },
+  {
+    "_id": "61682bf5e3a7d3c44f8b707b",
+    "titre": "duis irure ad reprehenderit consectetur",
+    "contenu": "Ex ea minim dolore et et Lorem ut anim ea dolor voluptate consectetur est."
+  }
+]
+*/
+
+
+
 
 const PORT = process.env.PORT || 5222;
 app.listen(PORT , () => console.log(`serveur express démarré sur le port ${PORT}`));
