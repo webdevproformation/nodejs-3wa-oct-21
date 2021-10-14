3e1d4a7dd8e4b3244a35a129769843e592b6ff9f
Malik H <malik.h@webdevpro.net>
2021-10-14 15:28:43 +0200

    correction

---
+++ jour4-template/data/infos.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+      "_id": "61682bf5e6dfd4b6594faa2d",
+      "titre": "do eu nisi voluptate occaecat",
+      "contenu": "Commodo sint ad aute aliquip aute cillum ad incididunt incididunt nostrud culpa proident."
+    },
+    {
+      "_id": "61682bf5855898e021007dd7",
+      "titre": "reprehenderit cillum ut veniam ullamco",
+      "contenu": "Cillum reprehenderit cillum enim cillum occaecat ullamco quis est esse commodo irure ea consectetur in."
+    },
+    {
+      "_id": "61682bf5e07bf1c3ff13df06",
+      "titre": "laboris sint officia eiusmod dolore",
+      "contenu": "Amet exercitation sint veniam fugiat eiusmod reprehenderit amet enim pariatur enim."
+    },
+    {
+      "_id": "61682bf58411e475e6e0d4f8",
+      "titre": "laboris proident commodo ea eu",
+      "contenu": "Duis cillum reprehenderit amet ea quis in."
+    },
+    {
+      "_id": "61682bf58f6ad826957550e8",
+      "titre": "ullamco anim consequat elit dolore",
+      "contenu": "Id pariatur ex ut culpa incididunt eu eiusmod."
+    },
+    {
+      "_id": "61682bf5e3a7d3c44f8b707b",
+      "titre": "duis irure ad reprehenderit consectetur",
+      "contenu": "Ex ea minim dolore et et Lorem ut anim ea dolor voluptate consectetur est."
+    }
+  ]
--- jour4-template/serveur.js
+++ jour4-template/serveur.js
@@ -6,6 +6,7 @@
 
 const express = require("express"); 
 const app = express();
+const data = require("./data/infos");
 
 app.set("views","views"); // permet de dire à express où chercher les fichiers de vue 
 
@@ -67,6 +68,13 @@ app.get("/loop", (req, rep) => {
     rep.render("07-loop" , produits);
 })
 
+app.get("/exo", (req, rep) => {
+    const liste = {
+        data : data
+    }
+    rep.render("08-tableau-de-bord" , liste);
+})
+
 // créer un nouvelle route /exo => appeler fichier 08-tableau-de-bord.pug
 
 // ce fichier va extends un fichier pug template back-office 
@@ -84,40 +92,6 @@ body
 // 08-tableau-de-bord.pug 
 // tableau <table> qui va contenir la liste d'articles suivantes (qui est envoyée depuis le fichier server.js)
 
-/*
-[
-  {
-    "_id": "61682bf5e6dfd4b6594faa2d",
-    "titre": "do eu nisi voluptate occaecat",
-    "contenu": "Commodo sint ad aute aliquip aute cillum ad incididunt incididunt nostrud culpa proident."
-  },
-  {
-    "_id": "61682bf5855898e021007dd7",
-    "titre": "reprehenderit cillum ut veniam ullamco",
-    "contenu": "Cillum reprehenderit cillum enim cillum occaecat ullamco quis est esse commodo irure ea consectetur in."
-  },
-  {
-    "_id": "61682bf5e07bf1c3ff13df06",
-    "titre": "laboris sint officia eiusmod dolore",
-    "contenu": "Amet exercitation sint veniam fugiat eiusmod reprehenderit amet enim pariatur enim."
-  },
-  {
-    "_id": "61682bf58411e475e6e0d4f8",
-    "titre": "laboris proident commodo ea eu",
-    "contenu": "Duis cillum reprehenderit amet ea quis in."
-  },
-  {
-    "_id": "61682bf58f6ad826957550e8",
-    "titre": "ullamco anim consequat elit dolore",
-    "contenu": "Id pariatur ex ut culpa incididunt eu eiusmod."
-  },
-  {
-    "_id": "61682bf5e3a7d3c44f8b707b",
-    "titre": "duis irure ad reprehenderit consectetur",
-    "contenu": "Ex ea minim dolore et et Lorem ut anim ea dolor voluptate consectetur est."
-  }
-]
-*/
 
 
 
