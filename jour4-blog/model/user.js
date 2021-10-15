2df249a8c573d9c511369813966d43fc54b1e3d7
Malik H <malik.h@webdevpro.net>
2021-10-15 17:10:33 +0200

    ajout jwt

--- jour4-blog/model/user.js
+++ jour4-blog/model/user.js
@@ -1,8 +1,20 @@
 const { Schema , model } = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const schemaUser = new Schema({
     login : {type : String , required : true},
     password : {type : String , required : true} 
 });
+
+schemaUser.methods.genererJWT = function(){
+    const profil = {
+        _id = this._id,
+        login : this.login
+    }
+    return jwt.sign(profil , process.env.JWT_SECRET)
+    // npm i jsonwebtoken
+
+}
 
 module.exports =  User = model("users", schemaUser) ;
